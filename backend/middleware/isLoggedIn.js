0191bbab32f97fe8ae0516acdfa5bec97181073b
ogsuryansh <suryansh1885@gmail.com>
2025-07-14 20:07:56 +0530

    updated islogin

--- backend/middleware/isLoggedIn.js
+++ backend/middleware/isLoggedIn.js
@@ -1,12 +1,7 @@
 function isLoggedIn(req, res, next) {
-  console.log("🔐 Middleware triggered");
-  console.log("🧪 req.isAuthenticated():", req.isAuthenticated?.());
-  console.log("🧑 req.user:", req.user);
-
-  if (req.isAuthenticated && req.isAuthenticated()) {
+  if (req.user) {
     return next();
   }
-
   res.redirect("/api/auth/google");
 }
 module.exports = isLoggedIn;
